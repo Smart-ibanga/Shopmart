042ae99f1cc0c58fc36877be3b9890e5f157a751
Smart-ibanga <ibangasmart@gmail.com>
2023-08-18 23:25:33 +0100

    header component1

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div className='container-xxl'>
+        <div className="row">
+          <div className="col-6">
+            <p className='text-white'>
+              
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </header>
   )
 }
 
