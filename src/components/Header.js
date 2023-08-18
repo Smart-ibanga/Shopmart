43ef037b69408fe39407f6d1fa8aa1619c02172b
Smart-ibanga <ibangasmart@gmail.com>
2023-08-18 23:30:05 +0100

    header component 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,14 @@ function Header() {
       <div className='container-xxl'>
         <div className="row">
           <div className="col-6">
-            <p className='text-white'>
-              
+            <p className='text-white'>Free shipping over $100 and free returns</p>
+          </div>
+          <div className="col-6">
+            <p className='text-end text-white'>
+              Hotline 
             </p>
           </div>
         </div>
-
       </div>
     </header>
   )
