237890db84cd4b6e24863252a792ae91bdc5f8c4
Smart-ibanga <ibangasmart@gmail.com>
2023-08-21 00:03:46 +0100

    header update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,23 @@
 import React from 'react'
 
-function Header() {
+const Header = () => {
   return (
-    <header>
-      <div className='container-xxl'>
+    <header className="header-top-strip py-3">
+      <div className="container-xxl">
         <div className="row">
           <div className="col-6">
-            <p className='text-white'>Free shipping over $100 and free returns</p>
+            <p>
+              Free shipping over $100 and Free returns</p>
           </div>
           <div className="col-6">
-            <p className='text-end text-white'>
-              Hotline 
+            <p className="text-end">
+              Hotline <a href="+234 8033320634">+234 8033320634</a>
             </p>
           </div>
         </div>
       </div>
     </header>
-  )
+  );
 }
 
 export default Header
